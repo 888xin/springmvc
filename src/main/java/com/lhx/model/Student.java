8cd1cb917dfd19f22d6aea4ecf6d34c178b06292
xin <888xin@sina.com>
2015-01-10 20:36:01 +0800

    null

---
+++ src/main/java/com/lhx/model/Student.java
@@ -0,0 +1,51 @@
+package com.lhx.model;
+
+/**
+ * Created by xin on 15-1-10 下午8:34
+ *
+ * @project springmvc
+ * @package com.lhx.model
+ * @Description
+ * @blog http://blog.csdn.net/u011439289
+ * @email 888xin@sina.com
+ * @github https://github.com/888xin
+ */
+
+public class Student {
+    private int id ;
+    private String name ;
+    private int age ;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
