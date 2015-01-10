861d28cbebeac0e9b02ad64c0caf910b39b214e1
xin <888xin@sina.com>
2015-01-10 20:39:57 +0800

    null

---
+++ src/main/java/com/lhx/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.lhx.controller;
+
+/**
+ * Created by xin on 15-1-10 下午8:39
+ *
+ * @project springmvc
+ * @package com.lhx.controller
+ * @Description
+ * @blog http://blog.csdn.net/u011439289
+ * @email 888xin@sina.com
+ * @github https://github.com/888xin
+ */
+
+public class StudentController {
+}
